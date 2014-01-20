c12770f3007ba85ec1e63f3cc8a4dca0ca5c11c6
liangjun.zhong <zhongliangjun1@gmail.com>
2014-01-21 00:39:37 +0800

    adjust

--- src/main/java/com/dianping/spider/business/movie/MoviePicUpdateTask.java
+++ src/main/java/com/dianping/spider/business/movie/MoviePicUpdateTask.java
@@ -10,7 +10,6 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
